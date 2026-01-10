fb742068b0985a881d320c4a539fb288a5346548
ecc1o <gudarenkobogdan@gmail.com>
2026-01-10 21:00:22 +0200

    Add BaseClient to reduce duplication

---
+++ NetSdrClientApp/Networking/BaseClient.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetSdrClientApp.Networking
+{
+    public abstract class BaseClient
+    {
+        protected CancellationTokenSource _cts;
+
+        public event EventHandler<byte[]>? MessageReceived;
+
+        protected void OnMessageReceived(byte[] data)
+        {
+            MessageReceived?.Invoke(this, data);
+        }
+
+        protected async Task RunListenerLoop(Func<CancellationToken, Task> listenAction)
+        {
+            try
+            {
+                _cts = new CancellationTokenSource();
+                Console.WriteLine("Starting listening for incoming messages.");
+
+                while (!_cts.Token.IsCancellationRequested)
+                {
+                    await listenAction(_cts.Token);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Normal shutdown
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in listening loop: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Listener stopped.");
+            }
+        }
+
+        protected void CancelToken()
+        {
+            _cts?.Cancel();
+            _cts = null;
+        }
+    }
+}
